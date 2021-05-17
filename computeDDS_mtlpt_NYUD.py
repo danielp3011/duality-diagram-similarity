e7c1666a06199a0f5e8f87c5f76eb1137ab52c5d
YannicV <y.vorpahl@gmx.de>
2021-05-17 15:02:24 +0200

    take out comments

--- computeDDS_mtlpt_NYUD.py
+++ computeDDS_mtlpt_NYUD.py
@@ -5,11 +5,7 @@ Compute DDS between Taskonomy tasks
 @author: kshitij
 """
 
-list_of_tasks = 'autoencoder curvature denoise edge2d edge3d \
-keypoint2d keypoint3d colorization \
-reshade rgb2depth rgb2mist rgb2sfnorm \
-room_layout segment25d segment2d vanishing_point \
-segmentsemantic class_1000 class_places inpainting_whole'
+list_of_tasks = 'depth normals'
 import numpy as np
 import os
 import glob
@@ -59,13 +55,15 @@ def main():
     parser.add_argument('-d','--dataset', help='image dataset to use for computing DDS: options are [pascal_5000, taskonomy_5000, nyuv2]', default = "taskonomy_5000", type=str)
     parser.add_argument('-fd','--feature_dir', help='path to saved features from taskonomy models', default = "../../../data2/yd", type=str)
     parser.add_argument('-sd','--save_dir', help='path to save the DDS results', default = "./results/DDScomparison_taskonomy", type=str)
-    parser.add_argument('-n','--num_images', help='number of images to compute DDS', default = 4500, type=int)
+    parser.add_argument('-n','--num_images', help='number of images to compute DDS', default = 200, type=int)
     args = vars(parser.parse_args())
 
     num_images = args['num_images']
     dataset = args['dataset']
-    features_filename = os.path.join(args['feature_dir'],"taskonomy_pascal_feats_" + args['dataset'] + ".npy")
-    save_dir = os.path.join(args['save_dir'],dataset)
+    features_filename = os.path.join(args['feature_dir'],"taskonomy_pascal_feats_" + args['dataset'] + ".npy") 
+    print("1: ", features_filename) 
+    save_dir = os.path.join(args['save_dir'],dataset) 
+    print("2: ", save_dir)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -106,7 +104,7 @@ def main():
     save_path = os.path.join(save_dir)
     dist_type = ['cosine']  #['pearson', 'euclidean', 'cosine']
     affinity_ablation = {}
-    for dist in (dist_type):
+    for dist in tqdm((dist_type)):
         affinity_ablation[dist]={}
         for feature_norm in (feature_norm_type):
             #affinity_matrix = np.zeros((len(task_list), len(task_list)), dtype = np.str)
@@ -127,19 +125,14 @@ def main():
                 #affinity_matrix[index1, index1] = 
                 x_train = StandardScaler().fit_transform(taskonomy_data[task1])
                 x_test = StandardScaler().fit_transform(taskonomy_data_test[task1])
-                #print(type(rdm(taskonomy_data[task1],dist)))
                 rdm_matrix_train[index1] = rdm(x_train,dist)
                 rdm_matrix_test[index1] = rdm(x_test,dist)
 
-                Path("./results_yd/yd_train/"+str(num_images)+"/").mkdir(parents=True, exist_ok=True) 
-                Path("./results_yd/yd_test/"+str(num_images)+"/").mkdir(parents=True, exist_ok=True) 
+                #Path(save_dir).mkdir(parents=True, exist_ok=True) 
+                #Path("./results_yd/yd_test/"+str(num_images)+"/").mkdir(parents=True, exist_ok=True) 
 
-                np.save("./results_yd/yd_train/"+str(num_images)+"/"+task1+"_yd_results", rdm_matrix_train[index1])
-                np.save("./results_yd/yd_test/"+str(num_images)+"/"+task1+"_yd_results", rdm_matrix_test[index1])
-
-                # create matlab format
-                # savemat("./results_yd/yd_train/"+task1+"_yd_results", rdm_matrix_train[index1])
-                # savemat("./results_yd/yd_test/"+task1+"_yd_results", rdm_matrix_test[index1])
+                np.save(save_dir + "/" + task1 + "_train.py", rdm_matrix_train[index1])
+                np.save(save_dir + "/" + task1 + "_test.py", rdm_matrix_test[index1])
 
             print("tasklist: ", task_list)
             print("len, tasklist: ", len(task_list))
@@ -152,8 +145,7 @@ def main():
             print("Method is ", method)
             print("Time taken is ", end - start)
     np.save(save_path, affinity_ablation)
-    #np.save("./results_yd/task_rdms_train", rdm_matrix_train)
-    #np.save("./results_yd/task_rdms_test", rdm_matrix_test)
+
 
 if __name__ == "__main__":
     main()
